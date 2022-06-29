9ef67df55f34ab10fb29cfe58a2b147884412b4f
alcss <fernandoalcalacasas@hotmail.com>
2022-06-29 14:59:06 -0500

    Comenzando a actualizar el diseño de login a la versión nueva

--- app/src/main/java/azoftware/com/whatsappro/MainActivity.java
+++ app/src/main/java/azoftware/com/whatsappro/MainActivity.java
@@ -19,8 +19,9 @@ public class MainActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
                 finally {
+                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                     //Intent intent = new Intent(MainActivity.this, ReporteActivity.class);
-                    Intent intent = new Intent(MainActivity.this, EstacionMeteorologica.class);
+                    //Intent intent = new Intent(MainActivity.this, EstacionMeteorologica.class);
                     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                     startActivity(intent);
                 }
