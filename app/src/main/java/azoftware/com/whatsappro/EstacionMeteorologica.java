adb1231617f09ce2bb6def2cc6d67277c5a7e519
alcss <fernandoalcalacasas@hotmail.com>
2022-07-03 23:09:39 -0500

    Terminada actualización de pantalla Estación Meteorológica acorde al diseño V2

--- app/src/main/java/azoftware/com/whatsappro/EstacionMeteorologica.java
+++ app/src/main/java/azoftware/com/whatsappro/EstacionMeteorologica.java
@@ -13,8 +13,10 @@ public class EstacionMeteorologica extends AppCompatActivity {
 
     //DECLARACIÓN DE CONSTANTES
     private final String LOGO_ESTACION ="https://i.ibb.co/hfc3kDB/image.png";
+    private final String IMAGEN_PERFIL="https://i.ibb.co/98H31Hw/foto-perfil-ejemplo.png";
     //DECALARCIÓN DE VARIABLES GLOBALES
     ImageView iVLogoEstacion;
+    ImageView iVFotoPerfil;
     Button iBBotones[];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,12 @@ public class EstacionMeteorologica extends AppCompatActivity {
 
         //ASOSIAMOS LOS OBJETOS DEL XML A LAS VARIABLES QUE LOS REPRESENTAN
         iVLogoEstacion = findViewById(R.id.iconoBarraEstación);
-        Glide.with(this).load(LOGO_ESTACION).error(R.drawable.ic_launcher_background).into(iVLogoEstacion);
+        iVFotoPerfil = findViewById(R.id.imagenPerfil);
+
+        //USAMOS GLIDE PARA APLICAR LA IMAGEN DE INTERNET EN EL IMAGE VIEW
+        Glide.with(this).load(LOGO_ESTACION).error(R.drawable.ic_launcher_foreground).into(iVLogoEstacion);
+        Glide.with(this).load(IMAGEN_PERFIL).error(R.drawable.ic_launcher_foreground).into(iVFotoPerfil);
+
+
     }
 }
